f7e3c765af3e8fd394d5affb542c64254f620e91
Sandip <pradhansandip031@gmail.com>
2026-08-17 13:14:46 +1000

    Let vs var; Constant variables

--- web/JavaScript learnings/file2.js
+++ web/JavaScript learnings/file2.js
@@ -1,1 +1,12 @@
-console.log("Hello World2");
+// let keyword instead of var keyword
+let firstName = "Sandip";
+console.log(firstName);
+// let firstName = "Sandip Pradhan"; // variable re-declaration error
+
+
+// constant variable declaration
+const pi= 3.14;
+let r=5;
+console.log(pi*r**2);
+// pi=3.14159; // constant variable re-declaration error 
+
