07272fe43395f4ea725dcb63e9fb5ee1f336395d
Sandip <pradhansandip031@gmail.com>
2026-08-17 13:05:39 +1000

    basics naming and variables

--- web/JavaScript learnings/file1.js
+++ web/JavaScript learnings/file1.js
@@ -1,2 +1,25 @@
+"use strict";
 // console. log  can print something on console
 console.log("Hello World"); 
+var firstName ="Sandip"; // variable declaration
+console.log(firstName);
+firstName ="Sandip Pradhan"; // variable re-declaration
+console.log(firstName);
+
+
+// rules for variabledeclaration
+// 1. A variable name must start with a letter, underscore (_), or dollar sign ($)  
+var X=10;
+console.log(X**2);
+var _X=20;
+console.log(_X**2);
+var $X=30;
+console.log($X**2);
+
+
+// 2. nospacce is allowed in variable name
+var last_Name ="Pradhan";// variable declaration with underscore
+console.log(last_Name);
+//convention for variable name
+// 1. camelCase and start with small letter
+var firstName ="Sandip";   
